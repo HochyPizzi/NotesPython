dd325693909eccfe0ea217bd6b6f6dc295a904cd
Demian <kilka228378@gmail.com>
2023-07-23 16:20:10 +0300

    Bug fixes

--- notes.py
+++ notes.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import date as d
 
 
 def hello():
@@ -17,11 +18,12 @@ def addingNote():
         idNote += 1
         name = input("Введите название заметки: ")
         body = input("Введите тело заметки: ")
-        date = datetime.now()
+        date = str(d.today()) + "," + str(datetime.now().hour) + ":" + str(datetime.now().minute)
         f.write(str(idNote) + "," + name + "," + body + "," + str(date) + "\n")
 
 
 def editingNote():
+    readingNote()
     searchData = input("Введите ID заметки для изменения: ")
     dataDictionary = {}
     with open("ListOfNotes.csv", "r", encoding="UTF-8") as f:
@@ -31,11 +33,12 @@ def editingNote():
             dataDictionary.pop(searchData)
     with open("ListOfNotes.csv", "w", encoding="UTF-8") as f:
         for key, value in dataDictionary.items():
-            f.write(str(key) + str(value).replace("[", "").replace("]", "").replace("'", "") + '\n')
+            f.write(str(key) + str(value).replace("[", "").replace("]", "").replace("'", ""))
     addingNote()
 
 
 def deletionNote():
+    readingNote()
     searchData = input("Введите ID заметки для удаления: ")
     dataDictionary = {}
     with open("ListOfNotes.csv", "r", encoding="UTF-8") as f:
@@ -45,7 +48,7 @@ def deletionNote():
             dataDictionary.pop(searchData)
     with open("ListOfNotes.csv", "w", encoding="UTF-8") as f:
         for key, value in dataDictionary.items():
-            f.write(str(key) + str(value).replace("[", "").replace("]", "").replace("'", "") + '\n')
+            f.write(str(key) + str(value).replace("[", "").replace("]", "").replace("'", ""))
 
 
 def menu():
@@ -55,7 +58,7 @@ def menu():
         print("Добавь свою первую заметку!")
         name = input("Введите название заметки: ")
         body = input("Введите тело заметки: ")
-        date = datetime.now()
+        date = str(d.today()) + "," + str(datetime.now().hour) + ":" + str(datetime.now().minute)
         f.write(str(idNote) + "," + name + "," + body + "," + str(date) + "\n")
     while True:
         print("1 - посмотреть все заметки\n2 - добавить заметку\n3 - изменить заметку\n4 - удалить заметку\n0 - выход")
